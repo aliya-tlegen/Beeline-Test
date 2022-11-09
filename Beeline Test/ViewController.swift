6c4ed05ecc42202212712044ba826d1520d1e911
aliya <tlegen.aliya@gmail.com>
2022-11-09 21:30:11 +0600

    initial commit

--- Beeline Test/ViewController.swift
+++ Beeline Test/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Private variables -
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+    
+    // MARK: - Lifecycle -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setupConstraints()
+        
     }
-
-
+    
+    // MARK: - Setup -
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    
+    // MARK: - Actions -
+    
+    
 }
 
