eeeb46a9d6eaaed00d5da5ea28c359dbe3d9a06c
aliya <tlegen.aliya@gmail.com>
2022-11-09 20:21:44 +0600

    Initial Commit

---
+++ Beeline Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beeline Test
+//
+//  Created by Алия Тлеген on 09.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
